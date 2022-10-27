f820145845f9c4458763079d593d4736fd795f6d
bryan20223 <116232964+bryan20223@users.noreply.github.com>
2022-10-27 00:58:48 -0500

    Examen
    Examen

---
+++ semanaDos1/Regristro.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semanaDos1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Regristro : ContentPage
+    {
+        private int v;
+
+        public Regristro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "el usuario conectado es " + usuario;
+        }
+
+        public Regristro(string usuario, int v) : this(usuario)
+        {
+            this.v = v;
+        }
+
+        private void btnInfo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double montoInicial = Convert.ToDouble(txtmontoInicial.Text);
+        
+            double cuota =((3000-montoInicial)/5)+(150);
+
+
+
+            if (montoInicial<3000 & montoInicial>0)
+            {
+            
+                Resultado.Text = cuota.ToString();
+                DisplayAlert("Mensaje de alerta", "Ingreso Correcto " + cuota + "\n " + montoInicial + "\n Aprobado: " + (cuota*5), "Cerrar");
+            }
+           else
+            {
+                DisplayAlert("Mensaje de alerta", "Error:" + nombre + "" + apellido + "la Cuota es incorrecta "+cuota, "Cerrar");
+
+            }
+            
+           
+        }
+
+        
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Resumen(txtcuota.Text, txtmontoInicial.Text, 30));
+
+            
+
+
+        }
+
+
+
+
+
+    }
+    
+}
---
+++ semanaDos1/login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semanaDos1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class loguin : ContentPage
+    {
+        public loguin()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnInicio_Clicked(object sender, EventArgs e)
+        {
+         await   Navigation.PushAsync(new Regristro(txtUser.Text,30));
+
+            string usuario = "Bryan2022";
+            string contrasena = "uisrael2022";
+            string tusuario = txtUser.Text;
+            string tcontrasena = txtContraseña.Text;
+
+            if (usuario == tusuario & contrasena == tcontrasena) 
+            {
+               await DisplayAlert("Alerta", "Usuario correcto", "Cerrar");
+                
+                
+            }
+            else
+            {
+               await DisplayAlert("Alerta", "Usuario no existe", "Cerrar");
+                
+
+            }
+
+
+        }
+    }
+}
